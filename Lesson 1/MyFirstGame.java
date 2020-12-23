02dbcf2a25ecb3e6013a970d116228c9b7bfe3f2
Dmitry Lazarev <dim@mars.local>
2020-12-23 15:36:37 +0100

    1.7 format fixing

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -10,16 +10,13 @@ public class MyFirstGame {
         do {
             if (playerNumber > randomNum) {
                 max = playerNumber;
-
                 System.out.println("Введенное вами число больше");
                 playerNumber = (playerNumber - min) / 2;
             } else if (playerNumber < randomNum) {
                 min = playerNumber;
-
                 System.out.println("Введенное вами число меньше");
                 playerNumber += (max - playerNumber) /  2;
             }
-
             System.out.println("Ваше число " + playerNumber);
         } while( playerNumber != randomNum);
         System.out.println("Вы угадали (" + randomNum + ") !");
