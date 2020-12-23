e11ecac44f985e2f80e26ea64bea1c23a306681f
Dmitry Lazarev <dim@mars.local>
2020-12-23 11:01:37 +0100

    file structure correction

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,13 +1,13 @@
 public class Variable {
     public static void main(String[] args) {
         boolean oldComputer = true;
-        byte    diskCount = 1;
-        short   cpuCores = 4;
-        int     memory = 16384;
-        long    diskAvailable = 1689190032;
-        float   cpuFreq = 2.3f;
-        double  diskFree = 21885424.0 * 100 / diskAvailable;
-        char    alpha = 'A';
+        byte diskCount = 1;
+        short cpuCores = 4;
+        int memory = 16384;
+        long diskAvailable = 1689190032;
+        float cpuFreq = 2.3f;
+        double diskFree = 21885424.0 * 100 / diskAvailable;
+        char alpha = 'A';
 
         System.out.println("Old comuter: " + oldComputer + "\nDisks: " + diskCount + "\nCores: " + cpuCores + "\nfrequency: " + cpuFreq + " GHz");
         System.out.println("Memory: " + memory + "kb\nDisk available: " + diskAvailable + "b");
