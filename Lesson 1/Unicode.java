6c992f017705926b7394ab59590e69f1fbfef61b
Dmitry Lazarev <dim@mars.local>
2020-12-23 12:11:05 +0100

    1.11 Unicode & MyFirstApp

---
+++ Lesson 1/Unicode.java
@@ -0,0 +1,13 @@
+class Unicode {
+    public static void main(String[] args) {
+        for (char i = 9398; i <= 10178 ; i++ ) {
+            System.out.println("Unicode code is: " + i);
+        }
+
+/*
+        for (char i = 33; i <= 126 ; i++ ) {
+            System.out.println("Unicode code is: " + i);
+        }
+    }
+*/
+}
