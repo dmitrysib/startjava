606e7d4c4858a66db12d5340ffa056504f72f48d
Dmitry Lazarev <dim@mars.local>
2020-12-24 16:16:13 +0100

    Lesson 2: Person.java

---
+++ Lesson 2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char sex = 'M';
+    String name = "Thomas";
+    int height = 155;
+    float weight = 120.1f;
+    int age = 50;
+
+    void walk() {
+        //
+    }
+
+    void seat() {
+        //
+    }
+
+    void run() {
+        //
+    }
+
+    void talk() {
+        //
+    }
+
+    void studyJava() {
+        //
+    }
+}
