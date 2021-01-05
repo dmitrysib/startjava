de46521ea16e80cdb724f541d7f09602bea900bd
dmitrysib <skydim@gmail.com>
2021-01-05 09:49:59 +0100

    - mathString переименовано в mathExpression  - usage переименовано в wrongMathExpression и перемещенно в конец  - создан метод для валидации входящего параметра

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -2,31 +2,16 @@ package com.startjava.lesson_4.calculator;
 
 public class Calculator {
 
-    public int usage() {
-        System.out.println("Математическое выражение должно выглядеть как \"Число<пробел>знак_математической_операции<пробел>Число\"");
-        System.out.println("Допустимый знак математической операции: \"+ - * / ^ %\"");
-        return 0;
-    }
+    public int calculate(String mathExpression) {
+        String[] parts = mathExpression.split(" ");
+        if (parts.length != 3)
+            return wrongMathExpression();
 
-    public int calculate(String mathString) {
-        String[] parts = mathString.split(" ");
-        if (parts.length != 3) {
-            return usage();
-        }
+        if (NonInteger(parts[0]) || NonInteger(parts[2]))
+            return wrongMathExpression();
 
-        int a, b;
-        try {
-            a = Integer.parseInt(parts[0].trim());
-        }
-        catch (NumberFormatException nfe) {
-            return usage();
-        }
-        try {
-            b = Integer.parseInt(parts[2].trim());
-        }
-        catch (NumberFormatException nfe) {
-            return usage();
-        }
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[2]);
 
         return switch (parts[1].charAt(0)) {
             case '+' -> a + b;
@@ -35,7 +20,22 @@ public class Calculator {
             case '/' -> a / b;
             case '^' -> (int) Math.pow(a, b);
             case '%' -> a % b;
-            default -> usage();
+            default -> wrongMathExpression();
         };
+    }
+
+    private int wrongMathExpression() {
+        System.out.println("Математическое выражение должно выглядеть как \"Число<пробел>знак_математической_операции<пробел>Число\"");
+        System.out.println("Допустимый знак математической операции: \"+ - * / ^ %\"");
+        return 0;
+    }
+
+    private boolean NonInteger(String str) {
+        try {
+            Integer.parseInt(str.trim());
+        } catch (NumberFormatException nfe) {
+            return true;
+        }
+        return false;
     }
 }
