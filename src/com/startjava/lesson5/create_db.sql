7d6a03b0d24db6de6fa1565734ad8586ecacd1aa
dmitrysib <skydim@gmail.com>
2021-01-11 08:50:00 +0100

    исправление форматирования

--- src/com/startjava/lesson5/create_db.sql
+++ src/com/startjava/lesson5/create_db.sql
@@ -1,12 +1,13 @@
 drop table if exists "Jaegers";
+
 create table "Jaegers" (
-  id serial primary key,
-  "modelName" varchar(255),
-  mark varchar(255),
-  height integer,
-  weight integer,
-  status varchar(255),
-  origin varchar(255),
-  launch date,
-  "kaijuKill" integer
+    id serial primary key,
+    modelName varchar(255),
+    mark varchar(255),
+    height integer,
+    weight integer,
+    status varchar(255),
+    origin varchar(255),
+    launch date,
+    kaijuKill integer
 );
